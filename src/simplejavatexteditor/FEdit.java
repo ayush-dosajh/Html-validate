11dae433748c999e37837ea8382b615ab0aeb68b
Chinmaya-03 <chinmaya.bansal.03@gmail.com>
2017-11-22 08:50:12 +0530

    Add files via upload

---
+++ src/simplejavatexteditor/FEdit.java
@@ -0,0 +1,28 @@
+/**
+ * @name        Java NotePad
+ * @package     notepad
+ * @file        UI.java
+ */
+ 
+ 
+
+package simplejavatexteditor;
+
+import javax.swing.JTextArea;
+import java.io.File;
+import java.io.PrintWriter;
+
+/**
+ * <h1>clear the  pad</h1>
+ */
+
+public class FEdit {
+
+    public static void clear(JTextArea textArea) {
+        textArea.setText("");
+    }
+
+    
+
+    
+}
